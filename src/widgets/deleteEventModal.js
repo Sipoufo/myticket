f9c3b527addbd238f6cc745d0fb231b288eae9e2
camcoder337 <camcoder337@gmail.com>
2023-12-24 14:41:19 +0100

    updated handleDeleteEvent

--- src/widgets/deleteEventModal.js
+++ src/widgets/deleteEventModal.js
@@ -1,8 +1,19 @@
-const deleteEventModal = ({ title, id, setShowModal, handleDelete }) => {
-    const handleClick = () => {
-        handleDelete(id);
-        // window.location.reload();
-    };
+import {deleteEvent} from "../services/eventService";
+const deleteEventModal = ({ title, eventid, setShowModal }) => {
+    const handleDeleteEvent = async (eventId) => {
+      try {
+          const response = await deleteEvent(eventId);
+          if (response.isError) {
+              console.error(response.message);
+          } else {
+              // Success Delete
+              console.log(response.message);
+              window.location.reload()
+          }
+      } catch (error) {
+          console.error(error);
+      }
+  };
 
     return (
         <div class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
@@ -27,7 +38,7 @@ const deleteEventModal = ({ title, id, setShowModal, handleDelete }) => {
           </div>
         </div>
         <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-          <button onClick={handleClick} type="button" class="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto">Delete</button>
+          <button onClick={() => handleDeleteEvent(eventid)} type="button" class="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto">Delete</button>
           <button onClick={() => setShowModal(false)} type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">Cancel</button>
         </div>
       </div>
@@ -35,7 +46,7 @@ const deleteEventModal = ({ title, id, setShowModal, handleDelete }) => {
   </div>
 </div>
 
-    );
+  );
 };
 
 export default deleteEventModal;
