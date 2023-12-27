4c0dba7b47cedc02b57e431c950ac60c3f619a73
camcoder337 <camcoder337@gmail.com>
2023-12-27 19:46:08 +0100

    added static user's table

--- src/pages/manager.js
+++ src/pages/manager.js
@@ -4,13 +4,24 @@ import Footer from "../components/footer";
 import Loading from "../components/loading";
 import { IoTicketOutline } from "react-icons/io5";
 import { useParams } from "react-router-dom";
-import { GetUserName } from "../services/token";
+import {FetchOneEvent} from "../services/eventService";
+
+
 
 const ManagerEvent = () => {
     const { eventId, isError, message } = useParams();
-    const [pageNumber, setPageNumber] = useState(1);
-    const [pageSize, setPageSize] = useState(10);
-    const [result, setResult] = useState(null);
+    const [event, setEvent] = useState([]);
+
+    const fetchOneEvent = async () => {
+          const data = await FetchOneEvent(eventId);
+          setEvent(data["data"]);
+    };
+    // const [pageNumber, setPageNumber] = useState(1);
+    // const [pageSize, setPageSize] = useState(10);
+    // const [result, setResult] = useState(null);
+    useEffect(() => {
+        fetchOneEvent();
+    }, [eventId]);
 
     return (
         <div className="w-full h-full flex flex-col overflow-y-auto gap-10 pb-40 md:pb-0">
@@ -26,55 +37,36 @@ const ManagerEvent = () => {
                 <div className="w-full h-full z-10 flex flex-col justify-center">
                     <Navbar />
                     <div className="flex justify-center items-center">
-                        <div className="flex flex-col gap-4 justify-between items-center text-white px-4 w-full md:w-10/12 max-w-screen-xl">
-                            <div className="bg-[#6569CC] flex w-28 h-28 border-4 border-white rounded-md justify-center items-center text-5xl font-medium">
-                                {GetUserName()[0]}
-                            </div>
-                            <h1 className="text-sm font-semibold">
-                                {GetUserName()}
-                            </h1>
-                        </div>
+                        <h1 class="mb-4 text-3xl font-extrabold leading-none tracking-tight text-dark-500 md:text-4xl lg:text-6xl dark:text-white pt-20">{event["name"]}</h1>
                     </div>
-                    <div className="absolute bottom-0 w-full flex justify-center">
-                        <div className="flex flex-row justify-center items-center text-white px-4 w-full md:w-6/12 max-w-screen-xl">
-                            <div className="flex flex-col gap-4 justify-between items-center text-white px-4 w-full md:w-10/12 max-w-screen-xl">
-                                <div className="bg-[#6569CC] flex w-16 h-16 border-2 border-white rounded-md justify-center items-center text-xl font-medium">
-                                    0
+                   
+                        <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+                            <div class="grid grid-cols-1 row-gap-8 md:grid-cols-3">
+                                <div class="text-center md:border-r px-2">
+                                <h6 class="text-4xl font-bold lg:text-5xl xl:text-6xl">203</h6>
+                                <p class="text-sm font-medium tracking-widest text-black-500 uppercase lg:text-base">
+                                    Ticket sold
+                                </p>
                                 </div>
-                                <h1 className="text-sm font-semibold">
-                                    Ticket sell
-                                </h1>
-                            </div>
-                            <div className="flex flex-col gap-4 justify-between items-center text-white px-4 w-full md:w-10/12 max-w-screen-xl">
-                                <div className="bg-[#6569CC] flex w-16 h-16 border-2 border-white rounded-md justify-center items-center text-xl font-medium">
-                                    0
+                                <div class="text-center md:border-r px-2">
+                                <h6 class="text-4xl font-bold lg:text-5xl xl:text-6xl">200</h6>
+                                <p class="text-sm font-medium tracking-widest text-black-500 uppercase lg:text-base">
+                                    Subscribers
+                                </p>
                                 </div>
-                                <h1 className="text-sm font-semibold">
-                                    Ticket sell
-                                </h1>
-                            </div>
-                            <div className="flex flex-col gap-4 justify-between items-center text-white px-4 w-full md:w-10/12 max-w-screen-xl">
-                                <div className="bg-[#6569CC] flex w-16 h-16 border-2 border-white rounded-md justify-center items-center text-xl font-medium">
-                                    0
+                                <div class="text-center px-2">
+                                <h6 class="text-4xl font-bold lg:text-5xl xl:text-6xl">101.5K</h6>
+                                <p class="text-sm font-medium tracking-widest text-black-500 uppercase lg:text-base">
+                                    Earnings
+                                </p>
                                 </div>
-                                <h1 className="text-sm font-semibold">
-                                    Ticket sell
-                                </h1>
                             </div>
-                            <div className="flex flex-col gap-4 justify-between items-center text-white px-4 w-full md:w-10/12 max-w-screen-xl">
-                                <div className="bg-[#6569CC] flex w-16 h-16 border-2 border-white rounded-md justify-center items-center text-xl font-medium">
-                                    0
-                                </div>
-                                <h1 className="text-sm font-semibold">
-                                    Ticket sell
-                                </h1>
-                            </div>
-                        </div>
-                    </div>
+                        </div> 
+                    
                 </div>
             </div>
 
-            <ul className={`flex flex-row gap-10 no-scrollbar mx-10`}>
+            {/* <ul className={`flex flex-row gap-10 no-scrollbar mx-10`}>
                 <li
                     className={`font-semibold pb-1 cursor-pointer text-gray-600`}
                 >
@@ -85,17 +77,207 @@ const ManagerEvent = () => {
                 >
                     Published
                 </li>
-            </ul>
+            </ul> */}
 
             {/* No Event */}
-            <div className={`flex justify-center`}>
+            {/* <div className={`flex justify-center`}>
                 <div className="h-96 flex flex-col gap-6 px-4 w-full md:w-10/12 max-w-screen-xl items-center justify-center">
                     <IoTicketOutline className="text-[10rem] text-gray-400" />
                     <p className="font-medium text-gray-600">
                         Oops, no events here!
                     </p>
                 </div>
+            </div> */}
+            {/* <!-- component --> */}
+            <div class="container mx-auto px-4 sm:px-8">
+        <div class="py-8">
+            <div>
+                <h2 class="text-2xl font-semibold leading-tight">Users</h2>
             </div>
+            <div class="my-2 flex sm:flex-row flex-col">
+                <div class="flex flex-row mb-1 sm:mb-0">
+                    <div class="relative">
+                        <select
+                            class="appearance-none h-full rounded-l border block appearance-none w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                            <option>4</option>
+                            <option>10</option>
+                            <option>20</option>
+                        </select>
+                        <div
+                            class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                            </svg>
+                        </div>
+                    </div>
+                    <div class="relative">
+                        <select
+                            class="appearance-none h-full rounded-r border-t sm:rounded-r-none sm:border-r-0 border-r border-b block appearance-none w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:border-l focus:border-r focus:bg-white focus:border-gray-500">
+                            <option>All</option>
+                            <option>TICKET-01</option>
+                            <option>TICKET-02</option>
+                        </select>
+                        <div
+                            class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                            </svg>
+                        </div>
+                    </div>
+                </div>
+                <div class="block relative">
+                    <span class="h-full absolute inset-y-0 left-0 flex items-center pl-2">
+                        <svg viewBox="0 0 24 24" class="h-4 w-4 fill-current text-gray-500">
+                            <path
+                                d="M10 4a6 6 0 100 12 6 6 0 000-12zm-8 6a8 8 0 1114.32 4.906l5.387 5.387a1 1 0 01-1.414 1.414l-5.387-5.387A8 8 0 012 10z">
+                            </path>
+                        </svg>
+                    </span>
+                    <input placeholder="Search"
+                        class="appearance-none rounded-r rounded-l sm:rounded-l-none border border-gray-400 border-b block pl-8 pr-6 py-2 w-full bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none" />
+                </div>
+            </div>
+            <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+                <div class="inline-block min-w-full shadow rounded-lg overflow-hidden">
+                    <table class="min-w-full leading-normal">
+                        <thead>
+                            <tr>
+                                <th
+                                    class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                    User
+                                </th>
+                                <th
+                                    class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                    TICKET
+                                </th>
+                                <th
+                                    class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                    Created at
+                                </th>
+                              
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                    <div class="flex items-center">
+                                        <div class="flex-shrink-0 w-10 h-10">
+                                            <img class="w-full h-full rounded-full"
+                                                src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80"
+                                                alt="" />
+                                        </div>
+                                        <div class="ml-3">
+                                            <p class="text-gray-900 whitespace-no-wrap">
+                                                Vera Carpenter
+                                            </p>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                    <p class="text-gray-900 whitespace-no-wrap">TICKET-02</p>
+                                </td>
+                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                    <p class="text-gray-900 whitespace-no-wrap">
+                                        Jan 21, 2020
+                                    </p>
+                                </td>
+
+                            </tr>
+                            <tr>
+                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                    <div class="flex items-center">
+                                        <div class="flex-shrink-0 w-10 h-10">
+                                            <img class="w-full h-full rounded-full"
+                                                src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80"
+                                                alt="" />
+                                        </div>
+                                        <div class="ml-3">
+                                            <p class="text-gray-900 whitespace-no-wrap">
+                                                Blake Bowman
+                                            </p>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                    <p class="text-gray-900 whitespace-no-wrap">TICKET-01</p>
+                                </td>
+                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                    <p class="text-gray-900 whitespace-no-wrap">
+                                        Jan 01, 2020
+                                    </p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                    <div class="flex items-center">
+                                        <div class="flex-shrink-0 w-10 h-10">
+                                            <img class="w-full h-full rounded-full"
+                                                src="https://images.unsplash.com/photo-1540845511934-7721dd7adec3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80"
+                                                alt="" />
+                                        </div>
+                                        <div class="ml-3">
+                                            <p class="text-gray-900 whitespace-no-wrap">
+                                                Dana Moore
+                                            </p>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                    <p class="text-gray-900 whitespace-no-wrap">TICKET-01</p>
+                                </td>
+                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                    <p class="text-gray-900 whitespace-no-wrap">
+                                        Jan 10, 2020
+                                    </p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="px-5 py-5 bg-white text-sm">
+                                    <div class="flex items-center">
+                                        <div class="flex-shrink-0 w-10 h-10">
+                                            <img class="w-full h-full rounded-full"
+                                                src="https://images.unsplash.com/photo-1522609925277-66fea332c575?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&h=160&w=160&q=80"
+                                                alt="" />
+                                        </div>
+                                        <div class="ml-3">
+                                            <p class="text-gray-900 whitespace-no-wrap">
+                                                Alonzo Cox
+                                            </p>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td class="px-5 py-5 bg-white text-sm">
+                                    <p class="text-gray-900 whitespace-no-wrap">TICKET-02</p>
+                                </td>
+                                <td class="px-5 py-5 bg-white text-sm">
+                                    <p class="text-gray-900 whitespace-no-wrap">Jan 18, 2020</p>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <div
+                        class="px-5 py-5 bg-white border-t flex flex-col xs:flex-row items-center xs:justify-between          ">
+                        <span class="text-xs xs:text-sm text-gray-900">
+                            Showing 1 to 4 of 50 Entries
+                        </span>
+                        <div class="inline-flex mt-2 xs:mt-0">
+                            <button
+                                class="text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded-l">
+                                Prev
+                            </button>
+                            <button
+                                class="text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded-r">
+                                Next
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+            
 
             {/* <div className="flex flex-col items-center">
                 <span className="text-sm">
