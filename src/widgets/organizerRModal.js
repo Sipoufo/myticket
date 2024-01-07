2cf91e7c4a8961c0f81463f34fe3b3250558f2d1
camcoder337 <camcoder337@gmail.com>
2024-01-07 10:41:59 +0100

    Add ORModal component

---
+++ src/widgets/organizerRModal.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import { IoMdClose } from "react-icons/io";
+import Loading from "../loading";
+import AlertMessage from "../../widgets/alert";
+
+const ORModal = ({setShowORModal}) => {
+
+
+};
+
+export default ORModal;
