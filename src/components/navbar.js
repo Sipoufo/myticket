23f8d26255bbc36f628f03548555be9cdab9a197
Sipoufo Yvan <58191884+Sipoufo@users.noreply.github.com>
2024-01-13 09:40:50 +0100

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -88,7 +88,7 @@ const Navbar = ({ token }) => {
                         alt="logo"
                     /> */}
                     <a href="/" className="font-semibold text-3xl text-white">
-                        CHAPCHAPTickets
+                        ChapChapTickets
                     </a>
                     <ul
                         className={`${
