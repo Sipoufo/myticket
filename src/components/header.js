467a5707f45856fa14adf42d2936b2c56712cc5b
camcoder337 <camcoder337@gmail.com>
2024-01-31 22:39:13 +0100

    Update event discovery text

--- src/components/header.js
+++ src/components/header.js
@@ -16,7 +16,7 @@ const Header = ({ token }) => {
                 <div className="flex justify-center">
                     <div className="flex flex-grow flex-col gap-6 items-center justify-center px-4 w-10/12 md:w-8/12 max-w-screen-xl">
                         <p className="text-3xl font-bold">
-                            Discover events across the world
+                            Discover events across the country
                         </p>
                         <form className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-6 text-black overflow-hidden gap-y-4">
                             {/* Search item */}
