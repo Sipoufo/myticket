fa3a535cb0d4e0e95332436937e443f24176270c
camcoder337 <camcoder337@gmail.com>
2024-02-04 15:06:54 +0100

    Refactor ticketPrint component to include hostUrl and showTicket props

--- src/components/Ticket/ticketPrint.js
+++ src/components/Ticket/ticketPrint.js
@@ -8,7 +8,9 @@ const TicketPrint = ({
   event, //Event object
   user, //Current User Object ~ User buying the Ticket
   ticket,//Ticket User for Information
-  handleClose
+  hostUrl,
+  showTicket,
+  resetSelectedTicket
 }) =>{
     const exportPDF = () => {
         const input = document.getElementById("Ticket");
@@ -45,42 +47,43 @@ const TicketPrint = ({
     };
 
   return(
-    <div className='h-full  max-w-md items-center flex flex-col overflow-y-auto z-50' >
+    <div className='h-full  max-w-md items-center flex flex-col overflow-y-auto z-100' >
       <div class="bg-white border rounded-lg shadow-lg px-6 py-8 max-w-md mx-auto" id="Ticket" ref={pdfRef}>
-    <h1 class="font-bold text-2xl my-4 text-center text-blue-600">Gangster Blow</h1>
+    <h1 class="font-bold text-2xl my-4 text-center text-blue-600">{ticket.ticket.event.name ?? 'Event Name'}</h1>
     <hr class="mb-2"/>
     <div class="flex justify-between mb-4">
         {/* <h1 class="text-lg font-bold">Invoice</h1> */}
-        <QRCode  size={50} bgColor='white' fgColor='black' value='localHostURL'/>
-        <div class="text-gray-700">
-            <div>📅Thu Jan 18 2024</div>
-            <div>🕒07:51:00</div>
-            <div>📍Yansoki</div>
+        <QRCode  size={50} bgColor='white' fgColor='black' value={`${hostUrl}/events/${ticket.ticket.event.eventId}`}/>
+        <div class="text-gray-700 text-right ">
+            <div>📅{new Date(ticket.ticket.event.startEvent).toLocaleDateString() +" - " +new Date(ticket.ticket.event.startEvent).toLocaleDateString() ?? 'Day Month Date'}</div>
+            <div>🕒{new Date(ticket.ticket.event.startEvent).toLocaleTimeString() + " - " + new Date(ticket.ticket.event.endEvent).toLocaleTimeString() ?? 'Hour Min Secs'}</div>
+            <div>📍{ticket.ticket.event.location ?? 'Location'}</div>
         </div>
     </div>
     <div class="flex justify-between mb-8">
-        <div>
+        <div className='text-left '>
         <h2 class="text-lg font-bold mb-4">Bill To:</h2>
-        <div class="text-gray-700 mb-2">John Doe</div>
-        <div class="text-gray-700">Sey Jeremi</div>
+        <div class="text-gray-700 mb-2">{ticket.user.firstName + " " + ticket.user.lastName ?? 'User Full Name'}</div>
+        <div class="text-gray-700 mb-2 ">{ticket.user.email ?? 'example@exampmle.co'}</div>
+        <div class="text-gray-700 mb-2 ">{ticket.user.phone ?? 'Phone Number'}</div>
         </div>
-        <div>
+        <div className='text-right'>
         <h2 class="text-lg font-bold mb-4">Ticket Info:</h2>
-        <div class="text-gray-700 mb-2">Name: Blow</div>
-        <div class="text-gray-700 mb-2">Type: Premium One</div>
-        <div class="text-gray-700 mb-2">N-#: 66259379234111</div>
+        <div class="text-gray-700 mb-2 font-semibold">{ticket.ticket.name ?? 'Ticket Name'}</div>
+        <div class="text-gray-700 mb-2">{ticket.ticket.ticketType.name ?? 'Ticket Type'}</div>
+        <div class="text-gray-700 mb-2">N-#:{ticket.ticketBuyId ?? '66259379234111'}</div>
         </div>
     </div>
     <div class="flex justify-between mb-4 gap-4">
-      <div>
-      <h2 class="text-lg font-bold mb-4">Organizer Info:</h2>
-        <div class="text-gray-700 mb-2">Name: John Doe</div>
-        <div class="text-gray-700 mb-2">Tel: 66259379</div>
+      <div className='text-left'>
+      <h2 class="text-lg font-bold mb-4">Organizer:</h2>
+        <div class="text-gray-700 mb-2">{ticket.ticket.event.organizer.firstName + " " + ticket.ticket.event.organizer.lastName ?? 'Organizer Full Name'}</div>
+        <div class="text-gray-700 mb-2">Tel: {ticket.ticket.event.organizer.phone ?? 'Phone'}</div>
       </div>
-      <div>
+      <div className='text-right'>
       <h2 class="text-lg font-bold mb-4">Command Info:</h2>
-        <div class="text-gray-700 mb-2">N-#: 66259379</div>
-        <div class="text-gray-700 mb-2">Date: Thu Jan 18 2024</div>
+        <div class="text-gray-700 mb-2">N-#:{ticket.ticketBuyId ?? '66259379234111'}</div>
+        <div class="text-gray-700 mb-2">{new Date(ticket.ticket.update_at).toDateString() ?? 'Command Date'}</div>
       </div>
         
     </div>
@@ -94,9 +97,9 @@ const TicketPrint = ({
         </thead>
         <tbody>
             <tr>
-                <td class="text-left text-gray-700">Premium One</td>
-                <td class="text-center font-bold text-gray-700">1</td>
-                <td class="text-right text-gray-700">$15000XAF</td>
+                <td class="text-left text-gray-700">{ticket.ticket.name ?? 'Ticket Name'}</td>
+                <td class="text-center font-bold text-gray-700">{ticket.ticket.number_place ?? '0'}</td>
+                <td class="text-right text-gray-700">{ticket.ticket.price ?? '0'}XAF</td>
             </tr>
 
         </tbody>
@@ -104,7 +107,7 @@ const TicketPrint = ({
             <tr>
                 <td class="text-left font-bold text-gray-700">Total</td>
                 <td class="text-center font-bold text-gray-700"/>
-                <td class="text-right font-bold text-gray-700">$15000XAF</td>
+                <td class="text-right font-bold text-gray-700">{ticket.ticket.number_place *  ticket.ticket.price ?? '0'}XAF</td>
             </tr>
         </tfoot>
     </table>
@@ -113,7 +116,11 @@ const TicketPrint = ({
 </div>
 
     <div className='flex justify-between w-full px-8'>
-    <button onClick={handleClose} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-8">Close</button>
+    <button onClick={()=>{
+      showTicket(false);
+      resetSelectedTicket();
+      }} 
+      className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-8">Close</button>
     <button onClick={printPDF} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-8">Print</button> 
     </div>
     </div>
